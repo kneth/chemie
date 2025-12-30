50081fd5cba43e3b04ae5b5aaa4e8f35b7ebe8e4
Kenneth Geisshirt <k@zigzak.net>
2025-12-30 09:01:15 +0100

    Add Lorentz as schema

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 pub trait Field {
     fn new(p: Vec<f64>) -> impl Field;
     fn df(&self, x: &Vec<f64>) -> Vec<f64>;
@@ -25,6 +27,29 @@ fn solver(end_time: f64, field: impl Field, x0: Vec<f64>, print_time: f64) {
     }
 }
 
+pub struct Lorentz {
+    pub sigma: f64,
+    pub rho: f64,
+    pub beta: f64
+}
+
+impl Field for Lorentz {
+    fn new(p: Vec<f64>) -> impl Field {
+        Lorentz {
+            sigma: p[0],
+            rho: p[1],
+            beta: p[2]
+        }
+    }
+
+    fn df(&self, x: &Vec<f64>) -> Vec<f64> {
+        let d0 = self.sigma*(x[1]-x[0]);
+        let d1 = x[0]*(self.rho-x[2]) - x[1];
+        let d2 = x[0]*x[1] - self.beta*x[2];
+        return vec![d0, d1, d2];
+    }
+}
+
 pub struct Brusselator {
     pub a: f64,
     pub b: f64
@@ -46,6 +71,18 @@ impl Field for Brusselator {
 }
 
 fn main() {
-    let b = Brusselator::new(vec![1.0, 3.0]);
-    solver(30.0, b, vec![1.0, 1.0], 0.1);
+    let args: Vec<String> = env::args().collect();
+    let schema = &args[1];
+
+    match schema.as_str() {
+        "brusselator" => { 
+            let b = Brusselator::new(vec![1.0, 3.0]);
+            solver(30.0, b, vec![1.0, 1.0], 0.1);
+        }
+        "lorentz" => {
+            let l = Lorentz::new(vec![10.0, 28.0, 8.0/3.0]);
+            solver(100.0, l, vec![0.9, 0.0, 0.0], 0.1);
+        }
+        _ => println!("Unknown schema")
+    }
 }
